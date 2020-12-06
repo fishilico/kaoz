348aeb7645717fab2740073c6735c87fb53a714c
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2020-12-06 10:10:17 +0100

    Use RSA 2048 certificates in test
    With RSA 1024, OpenSSL complains on Ubuntu 20.04:
    
        ssl.SSLError: [SSL: EE_KEY_TOO_SMALL] ee key too small (_ssl.c:2951)

--- kaoz/tests/certs/create-certs.sh
+++ kaoz/tests/certs/create-certs.sh
@@ -5,5 +5,5 @@ FILENAME="kaoz-example"
 KEYFILE="$FILENAME.key"
 CRTFILE="$FILENAME.crt"
 
-openssl req -new -x509 -newkey rsa:1024 -keyout "$KEYFILE" -out "$CRTFILE" -days 3650 -utf8 -nodes -config openssl.cnf && \
+openssl req -new -x509 -newkey rsa:2048 -keyout "$KEYFILE" -out "$CRTFILE" -days 3650 -utf8 -nodes -config openssl.cnf && \
 openssl x509 -noout -text -in "$CRTFILE"
